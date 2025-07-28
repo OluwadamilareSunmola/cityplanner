bde6ab96b908c9e7b1440315835c4ac80792f9e0
luciano093 <luparedes100@gmail.com>
2025-07-27 21:15:48 -0400

    Add map to Map page

---
+++ frontend/src/components/MapComponent.jsx
@@ -0,0 +1,26 @@
+// src/components/LeafletMap.js
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function MapComponent() {
+  return <>
+    <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+    url={`https://api.maptiler.com/maps/outdoor-v2/{z}/{x}/{y}.png?key=${import.meta.env.VITE_MAPTILER_API_KEY}`}
+    attribution='© MapTiler © OpenStreetMap contributors'
+  />
+      <CircleMarker
+        center={[40.7128, -74.0060]}
+        radius={8}
+        pathOptions={{ 
+          color: '#3388ff',
+          weight: 1,
+          fillColor: '#3388ff',
+          fillOpacity: 0.6
+        }}
+      >
+        <Popup>New York City</Popup>
+      </CircleMarker>
+    </MapContainer>
+  </>
+}
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar.jsx';
+import MapComponent from '../components/MapComponent.jsx';
 
-function Map() {
-
+export default function Map() {
   return (
-    <div className='wrapper-home'>
-      <Sidebar/>
+    <div className="wrapper-home">
+      <Sidebar />
+      <div className="map-container">
+        <MapComponent />
+      </div>
     </div>
   );
 }
-
-export default Map;
