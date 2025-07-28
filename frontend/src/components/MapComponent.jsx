7d6d997c3c665d4b5f023f96bfe460fa8d83809f
luciano093 <luparedes100@gmail.com>
2025-07-28 12:32:37 -0400

    Update map when getting user coordinates

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,16 +1,34 @@
 // src/components/LeafletMap.js
-import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, CircleMarker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export default function MapComponent() {
+import { useEffect } from 'react';
+
+// handle map center updates
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView(center, zoom);
+  }, [center, zoom, map]);
+
+  return;
+}
+
+export default function MapComponent({ coordinates }) {
+  if (!coordinates || coordinates.length !== 2) {
+    coordinates = [40.7128, -74.0060]; // default to New York City if no coordinates provided
+  }
+
   return <>
-    <MapContainer center={[40.7128, -74.0060]} zoom={13} style={{ height: '100%', width: '100%' }}>
+    <MapContainer center={coordinates} zoom={13} style={{ height: '100%', width: '100%' }}>
+      <ChangeView center={coordinates} zoom={13} />
       <TileLayer
     url={`https://api.maptiler.com/maps/outdoor-v2/{z}/{x}/{y}.png?key=${import.meta.env.VITE_MAPTILER_API_KEY}`}
     attribution='© MapTiler © OpenStreetMap contributors'
   />
       <CircleMarker
-        center={[40.7128, -74.0060]}
+        center={coordinates}
         radius={8}
         pathOptions={{ 
           color: '#3388ff',
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,37 +1,39 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Sidebar from '../components/Sidebar.jsx';
 import MapComponent from '../components/MapComponent.jsx';
 
 export default function Map() {
-    const [coordinates, setCoordinates] = useState([40.7128, -74.0060]); // default to New York City
-  
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-  
-        setCoordinates([lat, lon]);
-        
-        const res = fetch(`http://127.0.0.1:5000/search/geo?lat=${lat}&lon=${lon}&radius=5&unit=miles`);
-  
-        res.then(response => response.json())
-          .then(data => {
-            console.log(data);
-          });
-  
-      },
-      (error) => {
-          // display an error if we cant get the users position
-          console.error('Error getting user location:', error);
-      }
-  );
+  const [coordinates, setCoordinates] = useState([40.7128, -74.0060]); // default to New York City
+
+  useEffect(() => {
+      navigator.geolocation.getCurrentPosition(
+          (position) => {
+              const lat = position.coords.latitude;
+              const lon = position.coords.longitude;
+              setCoordinates([lat, lon]);
+          },
+          (error) => {
+              // display an error if we cant get the users position
+              console.error('Error getting user location:', error);
+          }
+      );
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`http://127.0.0.1:5000/search/geo?lat=${coordinates[0]}&lon=${coordinates[1]}&radius=50&unit=miles`);
+      const data = await res.json();
+      console.log(data);
+    };
+
+    fetchData();
+  }, [coordinates]);
 
   return (
     <div className="wrapper-home">
       <Sidebar />
       <div className="map-container">
-        <MapComponent />
+        <MapComponent coordinates={coordinates} />
       </div>
     </div>
   );
